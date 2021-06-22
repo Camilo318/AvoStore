42c89fcd28c9bf8c01b5c747d4a12a86ab9cbc80
Camilo318 <camiloa0318@gmail.com>
2021-06-22 10:05:22 -0500

    Add cart total section

--- components/CartList/index.tsx
+++ components/CartList/index.tsx
@@ -3,10 +3,14 @@ import CartItem from '../CartItem/index'
 import styles from './CartList.module.scss'
 import Message from '@components/Message'
 
+import { useCart } from '@components/AppProvider'
+
 interface Props {
   items: CartItemType[]
 }
 const CartList = ({ items }: Props) => {
+  const { total } = useCart()
+
   return (
     <section className={styles.cartList}>
       {items.length > 0 ? (
@@ -18,6 +22,12 @@ const CartList = ({ items }: Props) => {
           type='warning'
         />
       )}
+
+      <div className={styles.cartList__total}>
+        <p>
+          Total: <span>${total}</span>
+        </p>
+      </div>
     </section>
   )
 }
